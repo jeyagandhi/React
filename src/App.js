7889ce346d88d948868bc186842a6bc0fdde9470
Mageshpoopathi <mageshpoopathi208@gmail.com>
2024-08-21 13:38:06 +0530

    change in app2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <h2>Login page</h2>
+      <h2>Login page</h2>
+      <h2>Login page</h2>
       <h5>For Every User</h5>
      <Form/>
     </div>
